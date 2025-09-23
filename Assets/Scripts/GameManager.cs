b977bba5c596ba60d5841e2df089b07cc2432a96
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-09-23 09:14:05 +0700

    Bomb

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Text scoreText;
+    public Image fade;
 
+    private Blade blade;
+    private Spawner spawner;
     private int score = 0;
+
+    private void Awake()
+    {
+        blade = FindAnyObjectByType<Blade>();
+        spawner = FindAnyObjectByType<Spawner>();
+    }
 
     private void Start()
     {
@@ -14,13 +24,70 @@ public class GameManager : MonoBehaviour
 
     private void NewGame()
     {
+        Time.timeScale = 1f;
+        blade.enabled = true;
+        spawner.enabled = true;
         score = 0;
         scoreText.text = score.ToString();
+        ClearScene();
+
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsByType<Fruit>(FindObjectsSortMode.None);
+        foreach (Fruit fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsByType<Bomb>(FindObjectsSortMode.None);
+        foreach (Bomb bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
     }
 
     public void IncreaseScore(int points)
     {
         score += points;
         scoreText.text = score.ToString();
+    }
+
+    public void Explode()
+    {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fade.color = Color.Lerp(Color.clear, Color.white, t);
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        NewGame();
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fade.color = Color.Lerp(Color.white, Color.clear, t);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
     }
 }
